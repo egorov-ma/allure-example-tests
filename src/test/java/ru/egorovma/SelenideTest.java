a87709b4869bf39647b78df798959fffc8507930
Maksim Egorov <m.egorov193@gmail.com>
2023-12-21 13:00:58 +0400

    add testIssuesSearch

--- src/test/java/ru/egorovma/SelenideTest.java
+++ src/test/java/ru/egorovma/SelenideTest.java
@@ -1,2 +1,29 @@
-package ru.egorovma;public class SelenideTest {
+package ru.egorovma;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssuesSearch() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $("span[data-target='qbsearch-input.inputButtonText']").click();
+        $("#query-builder-test").setValue("eroshenkoam/allure-example").pressEnter();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(exist);
+    }
+
 }
